a52790612a3b04d99377ff0d9f026e2749bdf58b
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-13 16:41:57 +0000

    imported all the needed icons

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "../styles/Article.css"
 import avatar  from '../assets/avatar-michelle.jpg'
 import drawers from '../assets/drawers.jpg'
+// import {BiLogoFacebookSquare} from "react-icons/bi"//smaller icon
+import {AiFillFacebook} from "react-icons/ai"//bigger icon
+import {SiTwitter} from "react-icons/si"
+import {BsPinterest} from "react-icons/bs"
+import { PiShareFatFill } from "react-icons/pi";
 
 
 
