9cf57e27255fd73f1ae98ba0a6964d2f01fabf53
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-13 16:56:04 +0000

    added divs and content to Article.jsx

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -14,10 +14,29 @@ export const Article =()=>{
     return(
         <div className="main">
            <div className="picture">
-
+                <img src={drawers} alt="" />
            </div>
-           <div className="article">
-            your article will appear here 
+           <div className="articlePage">
+                <div>
+                    <h3>
+                    Shift the overall look and feel by adding these wonderful touches to furniture in your home
+                    </h3>
+                    <p>
+                        Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting.I've got some simple tips to help you make any room feel complete.
+                    </p>
+                </div>
+                <div className="source">
+                    <div className="profile">
+                     <div className="img"><img src={avatar} alt="" /></div>
+                     <div>
+                        <h3>Michelle Appleton</h3>
+                        <p>20 Jun 2020</p>
+                     </div>
+                    </div>
+                    <div className="share">
+                   <PiShareFatFill/>
+                    </div>
+                </div>
            </div>
         </div>
     )
